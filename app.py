8e336cc32ee9ceb6c652e7066cc62e59b53fdca4
Benjaminbg1 <benjaminbarreragarcia@gmail.com>
2025-04-13 23:06:33 -0400

    Update app.py
    11 Prueba de despliegue automático CI/CD desde GitHub

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class FishFeatures(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Prueba de despliegue automático CI/CD desde GitHub!V3"}
+    return {"message": "Prueba de despliegue automático CI/CD desde GitHub!V4"}
 
 
 @app.post("/predic/")
